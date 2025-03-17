5fc15f9b8f5cd9d4fd61fafd72fa01d489242fb3
Sang Ly Xuan <sang.lx@picontechlogy.com>
2025-03-17 17:56:55 +0700

    fix model and core/database

--- app/core/database/mixins/__init__.py
+++ app/core/database/mixins/__init__.py
@@ -1,20 +1,3 @@
-from .session import (
-    Base,
-    get_session,
-    reset_session_context,
-    session,
-    set_session_context,
-)
-from .standalone_session import standalone_session
-from .transactional import Propagation, Transactional
+from .timestamp import TimestampMixin
 
-__all__ = [
-    "Base",
-    "session",
-    "get_session",
-    "set_session_context",
-    "reset_session_context",
-    "standalone_session",
-    "Transactional",
-    "Propagation",
-]
+__all__ = ["TimestampMixin"]
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,3 @@
-from sqlmodel import SQLModel
-
-from .item import Item
 from .user import User
 
-__all__ = ["SQLModel", "User", "Item"]
+__all__ = ["User"]
